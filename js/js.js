d617c75862c2349abcb4d758e1740b95432161a6
Noora Jokinen <K8241@student.labranet.jamk.fi>
2017-11-06 18:53:25 +0200

    new files: json, js, html

---
+++ js/js.js
@@ -0,0 +1,55 @@
+function initMap() {
+    // create a map, point to the central of Finland
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 62.2426034, lng: 25.7472567},
+        zoom: 5
+    });
+    
+        // content string will display course info texts
+    var contentString = "";
+    // info window will display above content string
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+        // load and show markers
+    $.ajax({
+		url: 'json/alkot.json'
+    }).fail(function() {
+            console.log("fail!");
+    }).done(function(data) {
+        
+        // loop through all courses
+       $.each(data.stores, function(index,store) {
+            console.log(store.name);
+            
+                        // get position lat and lng
+           var storesLatLng = {lat: store.latitude, lng: store.longitude};
+            // marker
+            var marker = new google.maps.Marker({
+                position: storesLatLng,
+                map: map,
+                // include data to marker -> show in infowindow
+                title: store.name,
+                osoite: store.address,
+                postinro: store.postalCode,
+                city: store.city,
+                puhnro: store.phone,
+                auki0: store.OpenDay0,
+                auki1: store.OpenDay1
+                
+           });
+           
+                        // marker event handling
+           marker.addListener('click', function() {
+                infowindow.setContent(
+                    '<div id="content">'+
+                    '<h1 id="heading">'+this.title+'</h1>'+'<p>'+this.osoite+", "+this.postinro+" "+this.city+'</p>'+
+                    '<p>'+"Puhelin: "+this.puhnro+'</p>'+
+                    '<p>'+"Avoinna ma-pe: "+this.auki0+'</p>'
+                );
+                // show info window
+                infowindow.open(map, this);
+            });
+        }); // each
+    }); // ajax done
+}// init map
